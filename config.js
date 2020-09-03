abc0f68cd94140645978d3506784140fd5e569e0
kriztiank <kriztiank@gmail.com>
2020-09-03 12:30:03 +0200

    init

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGODB: 'mongodb+srv://kriztiank:EhSapxGC2N6DlDZi@cluster0.9tirf.mongodb.net/database?retryWrites=true&w=majority',
+  SECRET_KEY: 'some very secret key'
+}           
